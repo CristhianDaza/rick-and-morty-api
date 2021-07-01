2d3bff3667987a345ce07610e16bd11d73394907
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-01 11:56:35 -0500

    Agregando component filtro

---
+++ src/components/Filtros.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="filtros">
+    <p>Filtrar por:</p>
+    <b-field>
+      <b-select
+        placeholder="Status"
+        v-model="status"
+        @input="filtrarPersonajeStatus(status)"
+      >
+        <option value="alive">Alive</option>
+        <option value="dead">Dead</option>
+        <option value="unknown">Unknown</option>
+      </b-select>
+      <b-select
+        placeholder="Gender"
+        v-model="gender"
+        @input="filtrarPersonajeGender(gender)"
+      >
+        <option value="female">Female</option>
+        <option value="male">Male</option>
+        <option value="genderless">Genderless</option>
+        <option value="unknown">Unknown</option>
+      </b-select>
+    </b-field>
+  </div>
+</template>
+
+<script>
+import router from '@/router';
+
+export default {
+  name: 'filtros',
+  data() {
+    return {
+      gender: null,
+      status: this.$route.query.status || null,
+    }
+  },
+  methods: {
+    filtrarPersonajeGender(gender) {
+      console.log('click', gender)
+    },
+    filtrarPersonajeStatus(status) {
+      router.push({
+        path: '/',
+        query: {
+          status,
+        }
+      })
+    },
+  },
+}
+</script>
+
+<style>
+  .filtros{
+    display: flex;
+    align-items: center;
+  }
+
+  .filtros p {
+    font-size: 25px;
+  }
+
+  .filtros .control {
+    padding-left: 40px;
+    margin: 10px 0;
+  }
+</style>
